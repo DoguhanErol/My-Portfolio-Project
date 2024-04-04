929990e8de9ef6b0041d4d3959535d63cc74af97
Doguhan <DoguhanErolJob@gmail.com>
2024-04-04 14:30:27 +0300

    Some Tech Stack Component problems fixed.

--- frontend/src/components/aboutme/TechStackComponent.tsx
+++ frontend/src/components/aboutme/TechStackComponent.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import * as React from "react"
 import { useParams } from "react-router-dom";
+import globalVariables from "../../Config";
 import { fetchTechStacksByCategoryId } from "../../Requests";
 import { ITechStack } from "../../Types";
 import ErrorComponent from "../shared/ErrorComponent";
@@ -13,14 +14,11 @@ type TProps = {
 const TechStackComponent:React.FC<TProps> = (props:TProps) => {
   // Yeniden sorgu yapmak için useQuery hookunu her render'da yeniden çağırıyoruz
   const techStacks = useQuery<ITechStack[], Error, ITechStack[], ["techstacks"]>({
-    queryKey: ['techstacks', props.TechStackCategoryId],
+    queryKey: ['techstacks',props.TechStackCategoryId],
     queryFn: () => fetchTechStacksByCategoryId(props.TechStackCategoryId),
   });
 
-  React.useEffect(() => {
-    // props değiştiğinde yeni bir sorgu yapmak için useQuery hookunu yeniden çağırıyoruz
-    techStacks.refetch();
-  }, [props.TechStackCategoryId]);
+ 
 
   //Error Handling
   if (techStacks.isLoading || techStacks.isRefetching) {
@@ -38,12 +36,12 @@ const TechStackComponent:React.FC<TProps> = (props:TProps) => {
     <>
       <section className="flex flex-wrap -m-2">
         {techStacks.data && techStacks.data.length > 0 && techStacks.data.map((techStack) => (
-          <article className="p-2 lg:w-1/3 md:w-1/2 w-full" key={techStack.id}>
-            <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg">
-              <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80" />
+          <article className="p-2 lg:w-1/3 md:w-1/2 w-full" key={techStack.tech_stack_id}>
+            <div className="h-full flex items-center border-gray-600 bg-slate-700 border p-4 rounded-lg">
+              <img alt="team" className="w-16 h-16 bg-gray-100 bg-opacity-0 object-cover object-center flex-shrink-0 rounded-full mr-4" src={ globalVariables.baseUrl +techStack.tech_stack_image_path} />
               <div className="flex-grow">
                 <h2 className="text-white title-font font-medium">{techStack.tech_stack_title}</h2>
-                <p className="text-gray-600">{techStack.tech_stack_info}</p>
+                <p className="text-gray-400">{techStack.tech_stack_info}</p>
               </div>
             </div>
           </article>
